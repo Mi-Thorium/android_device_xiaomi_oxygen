5ae5ee4257b85d5004780e9bbd92bfc4d1f11c63
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-13 17:15:07 +0800

    Revert "ulysse: camera: Match metadata_data_t"
    This reverts commit a146d772c190f3b64b731aae115982c1f8290fb1.

--- camera/QCamera2/stack/common/cam_types.h
+++ camera/QCamera2/stack/common/cam_types.h
@@ -1955,8 +1955,6 @@ typedef enum {
 
     CAM_INTF_PARM_EZTUNE_CMD,
     CAM_INTF_PARM_INT_EVT,
-    CAM_INTF_XM_01,
-    CAM_INTF_XM_02,
 
     /* specific to HAL3 */
     /* Whether the metadata maps to a valid frame number */
