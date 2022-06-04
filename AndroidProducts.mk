c2f9d3c23c2cb6924b5877a6b08e51fa55215ac0
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-04 21:00:20 +0800

    oxygen: RevengeOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_oxygen-user \
     lineage_oxygen-userdebug \
     lineage_oxygen-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_oxygen.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_oxygen-user \
+    revengeos_oxygen-userdebug \
+    revengeos_oxygen-eng
