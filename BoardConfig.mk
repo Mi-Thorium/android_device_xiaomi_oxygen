a1dd563e816fb9291d13ff5b8062092230225f2a
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-26 02:27:59 +0800

    oxygen: Disable custom inode count calculation for SSI partitions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ BOARD_OXYGEN_DYNPART_PARTITION_LIST := $(ALL_PARTITIONS)
 
 # Partitions - reserved size
 $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 6144))
+    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := -1))
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 4096))
 
